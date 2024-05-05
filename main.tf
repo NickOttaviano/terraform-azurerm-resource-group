fec671565dffacceaeebad413f3934834a765251
Woohdini <nicko3312@gmail.com>
2024-05-05 18:53:36 -0400

    adding outputs and lock

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = lower(var.resource_group_name)
   location = var.location
   tags     = var.tags
+}
+
+resource "azurerm_management_lock" "rg_lock" {
+  count = var.lock_level == "" ? 0 : 1
+
+  name       = "${azurerm_resource_group.rg.name}-lock"
+  scope      = azurerm_resource_group.rg.id
+  lock_level = var.lock_level
+  notes      = "Resource group is locked with '${var.lock_level}' level."
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,9 @@ variable "tags" {
   description = "Resource group tags."
   type        = map(string)
 }
+
+variable "lock_level" {
+  description = "Lock level of the rg. Possible values are Empty (no lock), CanNotDelete and ReadOnly."
+  type        = string
+  default     = ""
+}
